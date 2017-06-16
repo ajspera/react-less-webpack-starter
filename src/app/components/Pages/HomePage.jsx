e839fb89af6c1cbe3344d8e87cf76c59ebe89b44
ajspera <speradev@gmail.com>
2017-06-16 00:53:50 -0600

    components for route pages

---
+++ src/app/components/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Page</h1>
+        <p>Hey there.</p>
+      </div>
+    )
+  }
+}
---
+++ src/app/components/Pages/SearchPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SearchForm from '../Forms/SearchForm.jsx';
+
+export default class SearchPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Search Page</h1>
+        <SearchForm />
+      </div>
+    )
+  }
+}
