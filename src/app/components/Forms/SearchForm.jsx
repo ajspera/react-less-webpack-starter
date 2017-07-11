8906aac0683c30ddf58d72f8a016adedaa8923c0
ajspera <speradev@gmail.com>
2017-07-11 14:51:29 -0600

    fix bugs for basic component usage

--- src/app/components/Forms/SearchForm.jsx
+++ src/app/components/Forms/SearchForm.jsx
@@ -3,36 +3,26 @@ import React from 'react';
 export default class SearchForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      value: ''
-    }
-
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(e) {
-    console.log(this)
-    try {
-      alert(this.state.value);
-    } catch(e) {
-      console.log(e)
-    }
-    e.preventDefault();
-    return false;
-  }
   handleChange(e) {
-    this.setState({value: e.target.value});
+    let obj = this.props.query;
+    obj[e.target.name] = e.target.value;
+    this.props.onChange(obj);
   }
   render() {
+
+    var query = this.props.query;
+    console.log(this.props)
+
     return (
       <div className="search-form">
         <form onSubmit={this.handleSubmit}>
           <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            Subreddit:
+            <input type="text" value={query.subreddit} name="subreddit" onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit" />
         </form>
       </div>
     )
--- src/app/components/Pages/SearchPage.jsx
+++ src/app/components/Pages/SearchPage.jsx
@@ -4,10 +4,13 @@ import SearchForm from '../Forms/SearchForm.jsx';
 
 export default class SearchPage extends React.Component {
   render() {
+    var query = {
+      subreddit: ''
+    };
     return (
       <div>
         <h1>Search Page</h1>
-        <SearchForm />
+        <SearchForm query={query} />
       </div>
     )
   }
