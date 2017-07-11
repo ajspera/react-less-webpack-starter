7d8830232b26c1feb1f649d8ffa65d11362765f8
ajspera <speradev@gmail.com>
2017-07-11 14:54:43 -0600

    base http

--- src/app/services/http.js
+++ src/app/services/http.js
@@ -2,3 +2,11 @@
 
 // superagent - https://visionmedia.github.io/superagent/ *
 // axios - https://github.com/mzabriskie/axios
+
+import axios from 'axios';
+
+var http = axios.create({
+  baseURL: 'https://reddit.com/r/'
+})
+
+export http;
